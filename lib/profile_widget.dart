043eeef4ae8eae70fb3459de267ef537f00b8b18
andrew535210099 <andrew.535210099@stu.untar.ac.id>
2023-06-21 20:42:05 +0700

    nambahin tulisan profile

--- lib/profile_widget.dart
+++ lib/profile_widget.dart
@@ -11,12 +11,31 @@ class UserProfile extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          SizedBox(height: 16), // Tambahkan jarak 16 pada bagian ini
-          CircleAvatar(
-            radius: 50,
-            backgroundImage: AssetImage('assets/images/gambar1.jpeg'),
+          SizedBox(height: 16),
+          Row(
+            children: [
+              SizedBox(width: 16),
+              Text(
+                'Profile',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ],
           ),
-          SizedBox(height: 16), // Tambahkan jarak 16 pada bagian ini
+          SizedBox(height: 16),
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('assets/images/gambar1.jpeg'),
+              ),
+            ],
+          ),
+          SizedBox(height: 16),
           Text(
             'Big Food Delivery',
             style: TextStyle(
